fcb5b8c86723ba100446c9aac95f7dd7616b71de
mohithsrao <mohithsrao@gmail.com>
2016-11-14 03:01:05 +0530

    Code Cleanup

--- ChessGame/ViewModels/MainWindowViewModel.cs
+++ ChessGame/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,6 @@
 ﻿using ChessElements;
 using ChessInfrastructure;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using static ChessInfrastructure.ChessEnums;
 
 namespace ChessGame.ViewModels
