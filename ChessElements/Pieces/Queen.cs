3d66014967712ae9ca54d1ca925b05437a8cd90d
mohithsrao <mohithsrao@gmail.com>
2016-11-28 01:57:48 +0530

    Added Movement logic for Queen

--- ChessElements/Pieces/Queen.cs
+++ ChessElements/Pieces/Queen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessElements.Extensions;
+using System;
 using System.Collections.Generic;
 using static ChessInfrastructure.ChessEnums;
 
@@ -16,9 +17,77 @@ namespace ChessElements.Pieces
 
         #region Public Methods
 
+        /// <summary>
+        /// Gets the move list for Queen
+        /// </summary>
+        /// <param name="tile"></param>
+        /// <returns></returns>
         public override List<Tile> GetMoveList(Tile tile)
         {
-            throw new NotImplementedException();
+            var list = new List<Tile>();
+            bool ppdCanMove = true, 
+                npdCanMove = true, 
+                pndCanMove = true, 
+                nndCanMove = true, 
+                pphCanMove = true, 
+                pnhCanMove = true, 
+                nphCanMove = true,
+                nnhCanMove = true;
+            for (int i = 1; i <= 8; i++)
+            {
+                //Diagonal Moves
+                if(ppdCanMove)
+                {
+                    var row = (int)tile.Row + i;
+                    var column = (int)tile.Column + i;
+                    ppdCanMove = tile.GetNextMove(list, row, column);
+                }
+                if (npdCanMove)
+                {
+                    var row = (int)tile.Row - i;
+                    var column = (int)tile.Column + i;
+                    npdCanMove = tile.GetNextMove(list, row, column);
+                }
+                if (pndCanMove)
+                {
+                    var row = (int)tile.Row + i;
+                    var column = (int)tile.Column - i;
+                    pndCanMove = tile.GetNextMove(list, row, column);
+                }
+                if (nndCanMove)
+                {
+                    var row = (int)tile.Row - i;
+                    var column = (int)tile.Column - i;
+                    nndCanMove = tile.GetNextMove(list, row, column);
+                }
+                //Right Angled moves
+                if (pphCanMove)
+                {
+                    var row = (int)tile.Row;
+                    var column = (int)tile.Column + i;
+                    pphCanMove = tile.GetNextMove(list, row, column);
+                }
+                if (pnhCanMove)
+                {
+                    var row = (int)tile.Row;
+                    var column = (int)tile.Column - i;
+                    pnhCanMove = tile.GetNextMove(list, row, column);
+                }
+                if (nphCanMove)
+                {
+                    var row = (int)tile.Row + i;
+                    var column = (int)tile.Column;
+                    nphCanMove = tile.GetNextMove(list, row, column);
+                }
+                if (nnhCanMove)
+                {
+                    var row = (int)tile.Row - i;
+                    var column = (int)tile.Column;
+                    nnhCanMove = tile.GetNextMove(list, row, column);
+                }
+            }
+
+            return list;
         }
 
         #endregion
